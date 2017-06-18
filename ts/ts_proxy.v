fbb9d6c97e30cd7cb0830fdda97fe7dffa3f66c0
Abylay Ospan <aospan@netup.ru>
2017-06-18 14:27:23 -0400

    TS stream generator add
    if 'insel' is 2'b11 then TS stream generated
    simple TS packets format:
    0x47 <-- TS sync byte
    0x01 <-- TS PID low
    0x77 <-- TS PID high
    0x1X <-- TS counter [0 .. 0xf]
    0xXX <-- (188 - 4) bytes filled by pattern. Pattern is incremented by 1 each TS packet.
    
    Signed-off-by: Abylay Ospan <aospan@netup.ru>

--- ts/ts_proxy.v
+++ ts/ts_proxy.v
@@ -1,8 +1,15 @@
+/* TS streams processing module 
+ * 
+ * (c) Abylay Ospan, 2017
+ * Joker Systems Inc.
+ * aospan@jokersys.com
+ */
+
 module ts_proxy (
 	input	wire		clk,
 	input	wire		reset,
 
-	/* input selector. 00 - DVB, 01 - DTMB, 10 - ATSC */
+	/* input selector. 00 - DVB, 01 - DTMB, 10 - ATSC  11 - TSGEN */
 	input	wire	[1:0]		insel,
 
 	/* wires from DTMB demod */
@@ -46,25 +53,33 @@ module ts_proxy (
 );
 
 /* state machine */
-reg [3:0] ts_samp_state = 4'b0000;
+reg [3:0] ts_samp_state;
 parameter ST_TS_IDLE=0, ST_TS_WRITE=1, ST_TS_COMMIT=2, ST_TS_WAIT_ACK=3, ST_TS_ROLLOVER=4, ST_TS_COMMIT2=5;
- 
-/* state machine for FIFO input */
-reg [3:0] ts_fifo_state = 4'b0000;
-parameter ST_FIFO_IDLE=0, ST_FIFO_WRITE=1;
 
-reg	[4:0] ts_dc	= 5'h00;
-reg	[10:0] cnt_p	= 10'h00;
-reg	[8:0] tslost_cnt = 8'h00;
-reg	[8:0] missed_ack = 8'h00;
+reg [3:0] fifo_state;
+parameter FIFO_IDLE=0, FIFO_WAIT=1;
+
+reg	[4:0] ts_dc;
+reg	[4:0]	fifo_dc;
+reg	[10:0] cnt_p;
+reg	[8:0] tslost_cnt;
+reg	[8:0] missed_ack;
 // reg	commit;
 wire	strt;
 wire	dval;
 reg	wren;
+reg	insert_marker;
 
 reg ack_2;
 reg ack_1;
-wire	[7:0]  fifo_data;
+wire	[7:0]		fifo_data;
+wire	[7:0]		ts_data;
+reg	[7:0]		tsgen_data;
+reg	[7:0]		tsgen_pattern;
+reg	[7:0]		tsgen_pos;
+reg	[3:0]		tsgen_counter;
+
+assign fifo_data = (insel == 2'b11) ? tsgen_data : ts_data;
 
 dvb_ts_selector tssel (
 	.rst (reset),
@@ -72,7 +87,7 @@ dvb_ts_selector tssel (
 	.clk (clk),
 	.strt (strt),
 	.dval (dval),
-	.data ( fifo_data /* ep3_usb_in_data */),
+	.data ( ts_data ),
 	.atsc_clock (atsc_clock),
 	.atsc_start (atsc_start),
 	.atsc_valid (atsc_valid),
@@ -90,24 +105,22 @@ dvb_ts_selector tssel (
 wire	[7:0]  fifo_q;
 reg	fifo_rdreq;
 reg	fifo_wrreq;
-wire	fifo_rdfull;
 wire	fifo_wrfull;
 wire	fifo_rdempty;
 reg	fifo_aclr;
+wire	almost_full;
 
-assign ep3_usb_in_data = fifo_q;
+assign ep3_usb_in_data = (insert_marker) ? 8'h33 : fifo_q;
 
 tsfifo tsfifo_inst (
-	.rdclk(clk),
-	.wrclk(clk),
+	.clock(clk),
 	.aclr(fifo_aclr),
 	.data(fifo_data),
-	.rdempty(fifo_rdempty),
+	.empty(fifo_rdempty),
 	.rdreq(fifo_rdreq),
 	.wrreq(fifo_wrreq),
 	.q(fifo_q),
-	.rdfull(fifo_rdfull),
-	.wrfull(fifo_wrfull)
+	.almost_full(almost_full)
 );
 
 assign	ep3_usb_in_addr = cnt_p;
@@ -116,7 +129,7 @@ assign	ep3_usb_in_commit_len = commit_len;
 assign	tslost = tslost_cnt;	
 assign	missed = missed_ack;	
 assign	state = ts_samp_state;
-assign	dc = cnt_p[3:0] /* ts_dc */;
+assign	dc = cnt_p[3:0];
 
 
 reg [31:0] source;
@@ -132,134 +145,129 @@ probe	probe_inst(
 */
 
 always @(posedge clk ) begin
-		ts_dc <= ts_dc + 1;
+	ts_dc <= ts_dc + 1;
+	fifo_dc <= fifo_dc + 1;
+	{ack_2, ack_1} <= {ack_1, ep3_usb_in_commit_ack };
+	wren <= 0;
+	fifo_rdreq <= 0;
+	fifo_aclr <= 0;
+	fifo_wrreq	<= 0;
+	insert_marker <= 0;		
+	probe[10:0] <= commit_len;
+	probe[12:11] <= insel;
 
-		{ack_2, ack_1} <= {ack_1, ep3_usb_in_commit_ack };
-		wren <= 0;
-		fifo_rdreq <= 0;
-		fifo_aclr <= 0;
-		fifo_wrreq	<= 0;
-		ep3_usb_in_commit <= 0;
-		
-		probe[10:0] <= commit_len;
-		probe[12:11] <= insel;
-
-		case (ts_fifo_state)
-	       ST_FIFO_IDLE:
-	       begin	
-	       if (dval) begin
-				
-		       if (fifo_wrfull) begin
-			       tslost_cnt <= tslost_cnt + 1;
-		       end
-		       else begin
-			       fifo_wrreq	<= 1;
-			       // ts_fifo_state <= ST_TS_WRITE;
-		      end
-	      end
-      end
-		/*  
-		ST_FIFO_WRITE:
+	case(fifo_state)
+	FIFO_IDLE:
+		case (insel)
+		2'b11: /* TSGEN selected */
 		begin
-	       fifo_wrreq <= 0;
-			 ts_fifo_state <= ST_TS_IDLE;
-		end */
-		endcase
-			 
-		 
-       case (ts_samp_state)
-	       ST_TS_IDLE:
-	       begin	
-				if (fifo_rdfull) begin
-					fifo_clean <= fifo_clean + 1;
-					fifo_aclr <= 1;
+			/* always keep FIFO almost full ! */
+			if (~almost_full) begin
+				
+				case(tsgen_pos)
+				8'h0: tsgen_data <= 8'h47; // TS sync byte 
+				8'h1: tsgen_data <= 8'h01; // PID high 
+				8'h2: tsgen_data <= 8'h77; // PID log 
+				8'h3:
+					begin
+						tsgen_data <= {4'h1,tsgen_counter}; // TS counter 
+						tsgen_counter <= tsgen_counter + 1; // new TS 
+						tsgen_pattern <= tsgen_pattern + 1; // new TS 
+					end
+				default: tsgen_data <= tsgen_pattern;
+				endcase
+				tsgen_pos <= tsgen_pos + 1'b1;
+				if (tsgen_pos == 8'd187)
+					tsgen_pos <= 0;
+				fifo_wrreq	<= 1;
+				
+				/*
+				// TODO: another type of traffic generator - sequential bytes 
+				if (tsgen_data == 8'h09)
+					tsgen_data <= 8'h0b;
+				else
+					tsgen_data <= tsgen_data + 1;
+				fifo_wrreq	<= 1;
+				*/
+			end
+		end
+		default:
+		begin
+			/* write real data from demods into FIFO if available */
+			if (dval) begin
+				if (almost_full) begin
+					tslost_cnt <= tslost_cnt + 1;
 				end
-				else if (~fifo_rdempty && ep3_usb_in_ready ) begin
-					probe[31] <= ~probe[31];
-					fifo_rdreq <= 1;
-					pkts_cnt <= pkts_cnt + 1;
-					// wren	<= 1;
-					ts_samp_state <= ST_TS_WRITE;
+				else begin
+					fifo_wrreq	<= 1;
 				end
 			end
-
-       ST_TS_WRITE:
-       begin
-			probe[30] <= ~probe[30];
-	       wren	<= 1;
-	       ts_samp_state <= ST_TS_COMMIT;
 		end
-			
-       ST_TS_COMMIT:
-       begin
-			
-			// ts_samp_state <= /* ST_TS_COMMIT */ ST_TS_WRITE;
-			
-					 
-	       /* ts_samp_state <= ST_TS_ROLLOVER;
-       end
-
-       ST_TS_ROLLOVER:
-       begin */
-			// if (cnt_p == 100)
-				// ep3_ext_buf_out_arm <= 1; //hack
+		endcase
+	endcase
+		
 		 
+	case (ts_samp_state)
+		ST_TS_IDLE:
+		begin	
+			if (~fifo_rdempty && ep3_usb_in_ready ) begin
+				probe[31] <= ~probe[31];
+				fifo_rdreq <= 1;
+				pkts_cnt <= pkts_cnt + 1;
+				ts_samp_state <= ST_TS_WRITE;
+			end
+		end
+		ST_TS_WRITE:
+		begin
+			if (cnt_p == commit_len - 2) begin
+				// insert_marker <= 1;
+			end
+			wren	<= 1;
+			ts_samp_state <= ST_TS_COMMIT;
+		end
+		ST_TS_COMMIT:
+		begin
 	       if (cnt_p == commit_len - 1) begin
-				probe[29] <= ~probe[29];
-	       // if (cnt_p == commit_len) begin
-		       cnt_p <= 0;
-		       // ts_samp_state <= ST_TS_COMMIT;
-				 ep3_usb_in_commit <= 1;
-				 ts_samp_state <= ST_TS_WAIT_ACK;
-				  ts_dc <= 0;
+				cnt_p <= 0;
+				ep3_usb_in_commit <= 1;
+				ts_samp_state <= ST_TS_WAIT_ACK;
+				ts_dc <= 0;
 	       end
 	       else begin
-				probe[28] <= ~probe[28];
-		       cnt_p <= cnt_p + 1;
-		       ts_samp_state <= ST_TS_IDLE;
+				cnt_p <= cnt_p + 1;
+				ts_samp_state <= ST_TS_IDLE;
 	       end
-       end
+		end
+		ST_TS_WAIT_ACK:
+		begin
+			if ( ts_dc > 5 )
+				missed_ack <= missed_ack + 1;
 
-       /* ST_TS_COMMIT:
-       begin
-	       ep3_usb_in_commit <= 1;			
-	       ts_samp_state <= ST_TS_WAIT_ACK;
-	       ts_dc <= 0;
-       end */
-
-       /*
-       ST_TS_COMMIT2:
-       begin
-	       if ( ts_dc > 1) begin
-		       commit <= 0;
-		       ts_dc <= 0;
-		       ts_samp_state <= ST_TS_WAIT_ACK;
-	       end
-       end */
-
-       ST_TS_WAIT_ACK:
-       begin
-				probe[27] <= ~probe[27];
-	       if ( ts_dc > 5 )
-		       missed_ack <= missed_ack + 1;
-
-	       if ( ts_dc > 6 || (ack_1 && ~ep3_usb_in_commit_ack )) begin	
+			if ( ts_dc > 6 || (ack_2 && ~ack_1 )) begin	
 		       ep3_usb_in_commit <= 0;
 		       acked <= acked + 1;
 		       ts_samp_state <= ST_TS_IDLE;
-				 probe[26] <= ~probe[26];
-	       end
-       end
+			end
+		end
 	endcase
 
 	if(reset) begin
 		ts_dc	<= 5'h00;
+		fifo_dc	<= 5'h00;
 		cnt_p	<= 8'h00;
 		wren	<= 0;
 		ep3_usb_in_commit <= 0;
 		tslost_cnt <= 0;
 		fifo_clean <= 0;
 		probe <= 0;
+		missed_ack <= 0;
+		ts_samp_state <= 0;
+		insert_marker <= 0;
+		fifo_state <= FIFO_IDLE;
+		tsgen_pattern <= 0;
+		tsgen_counter <= 0;
+		tsgen_pos <= 0;
+		tsgen_data <= 0;
 	end
 end
 
