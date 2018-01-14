d8802fb992789b7e57b7a51db6d2ace919610b55
Abylay Ospan <aospan@netup.ru>
2018-01-14 10:55:51 -0500

    testbench for USB phy and TS processing
    simulate phy ULPI:
      * reset
      * send/receive data
      * send tokens (SOF, DATA_IN)
      * receive data from EP3 (isoc)
    
    simulate TS data from ATSC demod
    
    Signed-off-by: Abylay Ospan <aospan@netup.ru>

--- testbench/ts_proxy_tb.v
+++ testbench/ts_proxy_tb.v
@@ -27,10 +27,31 @@ module ts_proxy_tb ();
 	/* ULPI from PHY */
 	reg	phy_ulpi_dir;
 	reg	phy_ulpi_nxt;
+	wire	phy_ulpi_stp;
+	wire	[7:0]   phy_ulpi_d;
+	reg	phy_ulpi_wr_en;
+	reg	[7:0]   phy_ulpi_wr_d;
+
+	parameter [3:0]	PID_TOKEN_OUT		= 4'hE,
+		PID_TOKEN_IN		= 4'h6,
+		PID_TOKEN_SOF		= 4'hA,
+		PID_TOKEN_SETUP		= 4'h2,
+		PID_TOKEN_PING		= 4'hB,
+		PID_DATA_0			= 4'hC,
+		PID_DATA_1			= 4'h4,
+		PID_DATA_2			= 4'h8,
+		PID_DATA_M			= 4'h0,
+		PID_HAND_ACK		= 4'hD,
+		PID_HAND_NAK		= 4'h5,
+		PID_HAND_STALL		= 4'h1,
+		PID_HAND_NYET		= 4'h9,
+		PID_SPEC_PREERR		= 4'h3,
+		PID_SPEC_SPLIT		= 4'h7,
+		PID_SPEC_LPM		= 4'hF;
 
 
 	ts_proxy DUT (
-		.clk(phy_clk /* clk */),
+		.clk(phy_clk ),
 		.atsc_clock(atsc_clock),
 		.atsc_start(atsc_start),
 		.atsc_valid(atsc_valid),
@@ -46,15 +67,25 @@ module ts_proxy_tb ();
 		.ep3_usb_in_commit_len(ep3_usb_in_commit_len),
 		.ep3_usb_in_ready(ep3_usb_in_ready),
 		.ep3_usb_in_commit_ack(ep3_usb_in_commit_ack),
-		.commit_len(11'd1020),
-		// .insel(2'b10),
-		.insel(2'b11), // TSGEN
+		.ts_ci_enable(0),
+		.commit_len(11'd1024), // isoc packet size
+		// .commit_len(11'd8), // isoc packet size
+		.insel(3'b010), // ATSC
+		// .insel(3'b101), // TSGEN mode 2
 		.reset(reset)
 	);
 
+	wire	[4:0]	next_crc5;
+	reg		[10:0]	crc5_data;
+
+	usb2_crc5 ic5 (
+		.c			( 5'h1F ),
+		.data		( crc5_data ),
+		.next_crc	( next_crc5 )
+	);
 
 	usb2_top USBTOP (
-		.ext_clk          ( phy_clk /* clk */ ),
+		.ext_clk          ( phy_clk ),
 		.reset_n          ( ~reset ),
 
 		.opt_disable_all  ( 1'b0 ),
@@ -65,9 +96,12 @@ module ts_proxy_tb ();
 
 		/* ULPI */
 		.phy_ulpi_dir		(phy_ulpi_dir),
+		.phy_ulpi_d		(phy_ulpi_d),
+		.phy_ulpi_nxt		(phy_ulpi_nxt),
+		.phy_ulpi_stp		(phy_ulpi_stp),
 
 		/* EP3 TS */
-		.ep3_ext_clk				( /* lg_clk */ /* clk_50 */ phy_clk),
+		.ep3_ext_clk		( phy_clk),
 		.ep3_buf_in_addr         ( ep3_usb_in_addr ),
 		.ep3_buf_in_data         ( ep3_usb_in_data ),
 		.ep3_buf_in_wren         ( ep3_usb_in_wren ),
@@ -77,53 +111,216 @@ module ts_proxy_tb ();
 		.ep3_buf_in_commit_ack   ( ep3_usb_in_commit_ack )
 	);
 
+	assign phy_ulpi_d = (phy_ulpi_wr_en) ? phy_ulpi_wr_d : 8'bz;
 
-	/* usb2_ep USBEP (
-		.phy_clk(phy_clk),
-		.rd_clk(phy_clk),
-		.reset_n(~reset),
-		.wr_clk(clk),
-		.buf_in_addr(ep3_usb_in_addr),
-		.buf_in_data(ep3_usb_in_data),
-		.buf_in_wren(ep3_usb_in_wren),
-		.buf_in_commit(ep3_usb_in_commit),
-		.buf_in_commit_len(ep3_usb_in_commit_len),
-		.buf_in_commit_ack(ep3_usb_in_commit_ack),
-		.buf_in_ready(ep3_usb_in_ready),
-		.fast_commit(0),
-		.buf_out_hasdata(buf_out_hasdata)
-	); */
+	reg	[3:0]	pid;
+	reg	[6:0]	addr; // device usb addr
+	reg	[3:0]	endp; // device usb endpoint
+
+	reg	[7:0]	bc;
+	reg	[7:0]	recv_data;
+	reg	[7:0]	send_data;
+	reg	[7:0]	send_data_link;
+	reg	[7:0]   ulpi_state;
+	reg	[7:0]   ulpi_state_next;
+	reg	[7:0]   ulpi_state_next_saved;
+	reg	[7:0]   ulpi_state_next_saved_token;
+	parameter [7:0]
+		ST_ULPI_RST = 8'd0,
+		ST_ULPI_RST_1 = 8'd1,
+		ST_ULPI_RST_2 = 8'd2,
+		ST_ULPI_IDLE = 8'd5,
+		ST_ULPI_RX = 8'd10,
+		ST_ULPI_TX = 8'd20,
+		ST_ULPI_TX_1 = 8'd21,
+		ST_ULPI_TX_LINK = 8'd30,
+		ST_ULPI_TX_LINK_1 = 8'd31,
+		ST_ULPI_TOKEN = 8'd40,
+		ST_ULPI_TOKEN_1 = 8'd41,
+		ST_ULPI_TOKEN_2 = 8'd42,
+		ST_ULPI_TOKEN_3 = 8'd43,
+		ST_ULPI_TOKEN_4 = 8'd44,
+		ST_ULPI_SEND_TOKEN = 8'd45,
+		ST_ULPI_ISOC = 8'd50,
+		ST_ULPI_ISOC_1 = 8'd51,
+		ST_ULPI_ISOC_RECV = 8'd52;
 
 	// create clock
 	always
-		// #5 clk = ~clk;  // 100mhz
-		// #3.8 clk = ~clk; // every ten nanoseconds invert
 		#10 clk = ~clk; // every ten nanoseconds invert
 
 	always
-		#3.8 atsc_clock = ~atsc_clock; // 3.8ns => 130 MHz
-		// #10 atsc_clock = ~atsc_clock; // every ten nanoseconds invert
+		#2.7 atsc_clock = ~atsc_clock; // 2.7ns => 180 MHz
+		// #3.8 atsc_clock = ~atsc_clock; // 3.8ns => 130 MHz
+		// #10 atsc_clock = ~atsc_clock; 
 
 	always
 		#8.33 phy_clk = ~phy_clk; // 60MHz ULPI
 
-	always @(posedge clk ) begin
-		if (ts_proxy_tb.DUT.almost_full)
-		begin
-			// simulate buffer cleanup
-			// force ts_proxy_tb.USBTOP.ipr.sel_endp = 2'b10;
-			force ts_proxy_tb.USBTOP.ipr.ep3_buf_out_arm = 1;
-		end
-		else begin
-			force ts_proxy_tb.USBTOP.ipr.ep3_buf_out_arm = 0;
-		end
-	end
+	/* USB transfers */
+	always @(posedge phy_clk ) begin
+		bc <= bc + 1'b1;
 
+		case(ulpi_state)
+			ST_ULPI_RST:
+			begin
+				addr <= 0;
+				endp <= 3; // isoc
+				ulpi_state <= ST_ULPI_RX;
+				ulpi_state_next <= ST_ULPI_RST_1;
+			end
+			ST_ULPI_RST_1:
+			begin
+				// send cmd to link
+				send_data <= 8'h0;
+				ulpi_state <= ST_ULPI_TX;
+				ulpi_state_next <= ST_ULPI_RST_2;
+			end
+			ST_ULPI_RST_2:
+			begin
+				ulpi_state <= ST_ULPI_RX;
+				ulpi_state_next <= ST_ULPI_ISOC;
+			end
+
+			ST_ULPI_IDLE:
+			begin
+			end
+
+			ST_ULPI_ISOC:
+			begin
+				// send SOF token
+				pid <= PID_TOKEN_SOF;
+				ulpi_state <= ST_ULPI_SEND_TOKEN;
+				ulpi_state_next <= ST_ULPI_ISOC_1;
+			end
+			ST_ULPI_ISOC_1:
+			begin
+				// send IN token
+				pid <= PID_TOKEN_IN;
+				ulpi_state <= ST_ULPI_SEND_TOKEN;
+				ulpi_state_next <= ST_ULPI_ISOC_RECV;
+			end
+			ST_ULPI_ISOC_RECV:
+			begin
+				// recv data from link
+				ulpi_state <= ST_ULPI_RX;
+				ulpi_state_next <= ST_ULPI_ISOC /* ST_ULPI_IDLE */;
+			end
+
+			/*** send token to link ***/
+			ST_ULPI_SEND_TOKEN:
+			begin
+				ulpi_state_next_saved_token <= ulpi_state_next;
+				// enable rxactive
+				send_data <= {2'b00,
+					2'b01 /* rx_event */,
+					2'b11 /* vbus */,
+					2'b00 /* linestate */};
+				ulpi_state <= ST_ULPI_TX_LINK;
+				ulpi_state_next <= ST_ULPI_TOKEN;
+			end
+
+			ST_ULPI_TOKEN:
+			begin
+				phy_ulpi_dir <= 1'b1;
+				phy_ulpi_nxt <= 1'b1;
+				phy_ulpi_wr_d <= {pid, ~pid};
+				phy_ulpi_wr_en <= 1'b1;
+				ulpi_state <= ST_ULPI_TOKEN_1;
+			end
+			ST_ULPI_TOKEN_1:
+			begin
+				crc5_data <= {endp[3], endp[2], endp[1], endp[0], addr};
+				phy_ulpi_wr_d <= {endp[0], addr};
+				ulpi_state <= ST_ULPI_TOKEN_2;
+			end
+			ST_ULPI_TOKEN_2:
+			begin
+				phy_ulpi_wr_d <= {next_crc5, endp[3], endp[2], endp[1]};
+				ulpi_state <= ST_ULPI_TOKEN_3;
+			end
+			ST_ULPI_TOKEN_3:
+			begin
+				phy_ulpi_dir <= 1'b0;
+				// disable rxactive
+				send_data <= {2'b00,
+					2'b00 /* rx_event */,
+					2'b11 /* vbus */,
+					2'b00 /* linestate */};
+				ulpi_state <= ST_ULPI_TX_LINK;
+				ulpi_state_next <= ST_ULPI_TOKEN_4;
+			end
+			ST_ULPI_TOKEN_4:
+			begin
+				ulpi_state <= ulpi_state_next_saved_token;
+			end
+
+			/*** RX data on ULPI and save to recv_data reg ***/
+			ST_ULPI_RX:
+			begin
+				phy_ulpi_dir <= 1'b0;
+				phy_ulpi_nxt <= 1'b1;
+				recv_data <= phy_ulpi_d;
+				if (phy_ulpi_stp) begin
+					ulpi_state <= ulpi_state_next;
+					phy_ulpi_dir <= 1'b0;
+				end
+			end
+
+			/*** TX data on link layer ***/
+			ST_ULPI_TX_LINK:
+			begin
+				// notify link about link layer data
+				// using rx_event field in rx cmd
+				send_data <= {2'b00,
+					2'b01 /* rx_event */,
+					2'b11 /* vbus */,
+					2'b00 /* linestate */};
+				ulpi_state_next_saved <= ulpi_state_next;
+				ulpi_state <= ST_ULPI_TX;
+				ulpi_state_next <= ST_ULPI_TX_LINK_1;
+				bc <= 0;
+			end
+			ST_ULPI_TX_LINK_1:
+			begin
+				// send actual data to link
+				// send_data <= send_data_link;
+				// ulpi_state <= ST_ULPI_TX;
+				// ulpi_state_next <= ulpi_state_next_saved;
+				ulpi_state <= ulpi_state_next_saved;
+			end
+
+			/*** TX data on ULPI from send_data reg ***/
+			ST_ULPI_TX:
+			begin
+				phy_ulpi_dir <= 1'b1;
+				phy_ulpi_nxt <= 1'b0;
+				/* link state:
+				* line_state  = [1:0];
+				vbus_state  = [3:2];
+				rx_event = [5:4]; */
+				phy_ulpi_wr_d <= send_data;
+				phy_ulpi_wr_en <= 1'b1;
+				ulpi_state <= ST_ULPI_TX_1;
+				bc <= 8'h0;
+			end
+			ST_ULPI_TX_1:
+			begin
+				if (bc == 3) begin
+					// hold data on line for 2-3 cycles
+					// transmission to link done
+					// phy_ulpi_dir left as high
+					ulpi_state <= ulpi_state_next;
+				end
+			end
+		endcase
+	end
 
 	/* serialize data from TS and write it to USB EP */
 	initial
 	begin
 		/* set all signals initial values */
+		ulpi_state = ST_ULPI_RST;
 		clk = 1'b0; // at time 0
 		atsc_clock = 1'b0; // at time 0
 		phy_clk = 1'b0; // at time 0
@@ -131,70 +328,77 @@ module ts_proxy_tb ();
 		atsc_valid = 1'b0; // at time 0
 		atsc_start = 1'b0; // at time 0
 		phy_ulpi_dir = 1'b0; // at time 0
+		phy_ulpi_nxt = 1'b0; // at time 0
+		phy_ulpi_wr_en = 1'b0;
+		phy_ulpi_wr_d = 8'h0;
 		indata = 8'h00;
 		i = 0;
 		reset = 1; // reset is active
 		#40 reset = 1'b0; // disable reset
 
-		@(negedge atsc_clock);
-		@(negedge atsc_clock);
+		// decrease waitime in ulpi from 10msec to 0usec
+		// this allows us to simulate smaller time range
+		force ts_proxy_tb.USBTOP.ia.reset_waittime = 0;
+
+		// TS sync 0x47
 		@(negedge atsc_clock);
 		@(negedge atsc_clock);
 		@(negedge atsc_clock);
 		indata = 8'h47;
+		atsc_start <= 1;
+		atsc_valid <= 1;
 		for( i = 0; i < 8; i=i+1 )
 		begin
-			atsc_start <= 1;
-			atsc_valid <= 1;
 			atsc_data <= indata[7-i];
 			@(negedge atsc_clock);
-			atsc_valid <= 0;
 			// @(negedge atsc_clock);
 		end
+		atsc_valid <= 0;
 		atsc_start <= 0;
 
+		// TS PID
 		@(negedge atsc_clock);
-		indata = 8'h55;
+		indata = 8'h1;
 		for( i = 0; i < 8; i=i+1 )
 		begin
 			atsc_valid <= 1;
 			atsc_data <= indata[7-i];
 			@(negedge atsc_clock);
 			atsc_valid <= 0;
-			// @(negedge atsc_clock);
 		end
 
-		bytes_sent <= 2;
-
-		for( bytes_sent = 0; bytes_sent < 4096; bytes_sent=bytes_sent+1 )
-		begin
-		// @(negedge atsc_clock);
-		indata = 8'hAA;
+		@(negedge atsc_clock);
+		indata = 8'h77;
 		for( i = 0; i < 8; i=i+1 )
 		begin
-			atsc_start <= 0;
 			atsc_valid <= 1;
 			atsc_data <= indata[7-i];
 			@(negedge atsc_clock);
-			// atsc_valid <= 0;
-			// @(negedge atsc_clock);
+			atsc_valid <= 0;
 		end
 
-		/* TODO:read usb isoc from EP3 */
-		phy_ulpi_dir <= 1'b1;
-		phy_ulpi_nxt <= 1'b1;
+		// TS counter
+		@(negedge atsc_clock);
+		indata = 8'h10;
+		for( i = 0; i < 8; i=i+1 )
+		begin
+			atsc_valid <= 1;
+			atsc_data <= indata[7-i];
+			@(negedge atsc_clock);
+			atsc_valid <= 0;
 		end
-	end
 
-	/* reading data from usb ep */
-	/* 
-	initial
-	begin
-		always @(posedge clk ) begin
-			if (buf_out_hasdata)
+		indata = 8'h0;
+		for( bytes_sent = 0; bytes_sent < 32768; bytes_sent=bytes_sent+1 )
+		begin
+			indata = indata + 1;
+			for( i = 0; i < 8; i=i+1 )
 			begin
+				atsc_start <= 0;
+				atsc_valid <= 1;
+				atsc_data <= indata[7-i];
+				@(negedge atsc_clock);
 			end
 		end
 	end
-	*/
 endmodule
